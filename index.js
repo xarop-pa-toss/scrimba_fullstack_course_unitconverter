2de04197120b71953179781747df74d575b00aa4
xarop-pa-toss <rpgdsantos@gmail.com>
2025-11-29 23:30:37 +0000

    Style light mode. Start with dynamic sections in JS

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+let sectionDescriptions = [
+    {
+        type: "length",
+        h3: "Length (Meter/Feet)",
+        p: "20 meters = 65.616 feet | 20 feet = 6.096 meters"
+    },
+    {
+        type: "volume",
+        h3: "Volume (Liters/Gallons)",
+        p: "20 liters = 5.284 gallons | 20 gallons = 75.708 liters"
+    },
+    {
+        type: "mass",
+        h3: "Mass (Kilograms/Pounds)",
+        p: "20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos"
+    }
+]
